4fe6e43f083bb3cbec84edecb620004fe7704296
Carlos Alvarez <carl.alvarez.arg@gmail.com>
2023-01-16 22:10:55 -0300

    correciones y mats
    Se agregaron correcciones y materiales

--- Movimiento+AlvarezCarlos/Assets/Scripts/movimientoNaranja.cs
+++ Movimiento+AlvarezCarlos/Assets/Scripts/movimientoNaranja.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class movimientoNaranja : MonoBehaviour
 {
     public float speed = 0.005f;
     public Transform usteEscalemelo;
+    public Vector3 pos;
 
     // Start is called before the first frame update
     void Start()
@@ -16,9 +18,11 @@ public class movimientoNaranja : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(0.5f, 0, 0) * speed * Time.deltaTime;
+        transform.position += pos * speed * Time.deltaTime;
 
         usteEscalemelo.localScale += usteEscalemelo.localScale * 0.0003f;
+
+        transform.Rotate(0, 0.5f, 0);
 
         Debug.Log("Estoy creciendo en " + usteEscalemelo.localScale.x + " por frame");
     }
